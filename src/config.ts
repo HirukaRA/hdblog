3169ba6e80c8e278802b5682073dd3a4f78135a4
Hiruka Ransana <168963519+HirukaRA@users.noreply.github.com>
2024-06-17 07:50:53 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -35,7 +35,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari',     // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/HirukaRA/hdevs-blog-01',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
@@ -43,8 +43,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'Hiruka Ransana',
+  bio: 'I am just a regular persong interested in Web Developing and stuff.',
   links: [
     {
       name: 'Twitter',
